679fab3ac99f6ba3ae3986d8e69a8a4fb4b98906
Carlos Medina <hola@medina.dev>
2026-02-17 14:23:08 -0600

    refactor: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,9 +12,9 @@ export default defineConfig({
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
         assetFileNames: `assets/[name].[ext]`,
-        globals: { 'ez_chatbot_settings': 'ez_chatbot_settings' }
+        globals: { ez_chatbot_settings: 'ez_chatbot_settings' },
       },
     },
   },
-  eslintConfigPrettier
+  eslintConfigPrettier,
 })
